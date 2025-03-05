09c174936b733010e3aeaa63a66ef4b9116ad722
RichardGL11 <richardgreghilammenda@hotmail.com>
2025-03-05 18:50:29 -0300

    tests: Validation Tests for creating a product

--- tests/Feature/Http/Livewire/Products/CreateProductTest.php
+++ tests/Feature/Http/Livewire/Products/CreateProductTest.php
@@ -31,3 +31,74 @@ $image =  UploadedFile::fake()->image('image.jpg',100,100);
 
 });
 
+describe('validation tests', function () {
+    test('name field', function ($rule,$value){
+        $image =  UploadedFile::fake()->image('image.jpg',100,100);
+        Livewire::actingAs(User::factory()->create())
+            ->test(CreateProduct::class)
+            ->set('name',$value)
+            ->set('description','description')
+            ->assertSet('description','description')
+            ->set('price', '100.00')
+            ->assertSet('price', '100.00')
+            ->set('image',$image)
+            ->call('save')
+            ->assertHasErrors(['name' => $rule]);
+    })->with([
+        'required' => ['required', ''],
+        'min'      => ['min:3','aa'],
+        'max'      => ['max:255',str_repeat('a',256)],
+    ]);
+    test('description field', function ($rule,$value){
+        $image =  UploadedFile::fake()->image('image.jpg',100,100);
+        Livewire::actingAs(User::factory()->create())
+            ->test(CreateProduct::class)
+            ->set('name','name')
+            ->assertSet('name','name')
+            ->set('description',$value)
+            ->set('price', '100.00')
+            ->assertSet('price', '100.00')
+            ->set('image',$image)
+            ->call('save')
+            ->assertHasErrors(['description' => $rule]);
+    })->with([
+        'required' => ['required', ''],
+        'min'      => ['min:3','aa'],
+        'max'      => ['max:255',str_repeat('a',256)],
+    ]);
+
+    test('price field', function ($rule,$value){
+        $image =  UploadedFile::fake()->image('image.jpg',100,100);
+        Livewire::actingAs(User::factory()->create())
+            ->test(CreateProduct::class)
+            ->set('name','name')
+            ->assertSet('name','name')
+            ->set('description','description')
+            ->assertSet('description','description')
+            ->set('price', $value)
+            ->set('image',$image)
+            ->call('save')
+            ->assertHasErrors(['price' => $rule]);
+    })->with([
+        'required'          => ['required', ''],
+        'numeric'           => ['numeric','aa'],
+        'greater than 0'    => ['gt:0',-1],
+    ]);
+
+    test('image field', function ($rule,$value){
+        Livewire::actingAs(User::factory()->create())
+            ->test(CreateProduct::class)
+            ->set('name','name')
+            ->assertSet('name','name')
+            ->set('description','description')
+            ->assertSet('description','description')
+            ->set('price', '100.00')
+            ->assertSet('price', '100.00')
+            ->set('image',$value)
+            ->call('save')
+            ->assertHasErrors(['image' => $rule]);
+    })->with([
+        'file'       => ['file', 'notafile'],
+        'mimes'      => ['mimes','something.svg'],
+    ]);
+});
