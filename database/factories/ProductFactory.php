1696dedecd5847de42b9107c1acecb805bdf83e3
RichardGL11 <richardgreghilammenda@hotmail.com>
2025-03-06 18:30:40 -0300

    refactor: adding fields for product factory

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -19,8 +19,12 @@ class ProductFactory extends Factory
         return [
             'name' => $this->faker->name(),
             'description' => $this->faker->text(),
-            'price' => $this->faker->randomFloat(2, 10),
+            'price' => $this->faker->randomFloat(2, 1),
             'image' => $this->faker->imageUrl(),
+            'height' => $this->faker->randomFloat(2, 1),
+            'width' => $this->faker->randomFloat(2, 1),
+            'length' => $this->faker->randomFloat(2, 1),
+            'weight' => $this->faker->randomFloat(2, 1),
         ];
     }
 }
