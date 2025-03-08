83619f50260273a3f4d10dd476c008fd039b0213
RichardGL11 <richardgreghilammenda@hotmail.com>
2025-03-08 14:43:25 -0300

    test: Only the user can see their shopping Cart

--- tests/Feature/Http/Livewire/ShoppingCart/ShoppingCartTest.php
+++ tests/Feature/Http/Livewire/ShoppingCart/ShoppingCartTest.php
@@ -2,6 +2,7 @@
 
 use App\Livewire\ShoppingCart\ShoppingCart;
 use Livewire\Livewire;
+use function Pest\Laravel\actingAs;
 
 it('should add a product to the shopping cart',function() {
     $product1 =\App\Models\Product::factory()->create();
@@ -40,4 +41,34 @@ it('should remove a product to the shopping cart',function() {
    $livewire->call('removeFromCart', $product1->id);
    $livewire->assertdontsee($product1->name);
    $livewire->assertdontsee($product1->description);
+});
+test('Only the user can see their shopping Cart',function() {
+    $product1 =\App\Models\Product::factory()->create();
+    $product2 =\App\Models\Product::factory()->create();
+    $user1 = \App\Models\User::factory()->create();
+    $user2 = \App\Models\User::factory()->create();
+
+    actingAs($user1);
+
+   $livewire = Livewire::test(ShoppingCart::class)
+    ->call('addToCart', $product1)
+    ->call('addToCart', $product2);
+
+   $livewire->assertSee($product1->name);
+   $livewire->assertSee($product1->price);
+   $livewire->assertSee($product1->description);
+   $livewire->assertSee($product2->name);
+   $livewire->assertSee($product2->price);
+   $livewire->assertSee($product2->description);
+
+    $livewire2 = Livewire::actingAs($user2)
+                ->test(ShoppingCart::class);
+
+    $livewire2->assertDontSee($product1->name);
+    $livewire2->assertDontSee($product1->price);
+    $livewire2->assertDontSee($product1->description);
+    $livewire2->assertDontSee($product2->name);
+    $livewire2->assertDontSee($product2->price);
+    $livewire2->assertDontSee($product2->description);
+
 });
