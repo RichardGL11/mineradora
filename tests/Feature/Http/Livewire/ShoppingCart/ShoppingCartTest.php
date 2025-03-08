30a085dbd215f9b34e9d8def5a9d7d83cc2d788e
RichardGL11 <richardgreghilammenda@hotmail.com>
2025-03-08 14:37:14 -0300

    test: should be able to remove a product from the shopping cart

--- tests/Feature/Http/Livewire/ShoppingCart/ShoppingCartTest.php
+++ tests/Feature/Http/Livewire/ShoppingCart/ShoppingCartTest.php
@@ -3,7 +3,7 @@
 use App\Livewire\ShoppingCart\ShoppingCart;
 use Livewire\Livewire;
 
-it('should add a item to the shopping cart',function() {
+it('should add a product to the shopping cart',function() {
     $product1 =\App\Models\Product::factory()->create();
     $product2 =\App\Models\Product::factory()->create();
 
@@ -19,4 +19,25 @@ it('should add a item to the shopping cart',function() {
    $livewire->assertSee($product2->price);
    $livewire->assertSee($product2->description);
 
+});
+
+it('should remove a product to the shopping cart',function() {
+    $product1 =\App\Models\Product::factory()->create();
+    $product2 =\App\Models\Product::factory()->create();
+
+   $livewire = Livewire::actingAs(\App\Models\User::factory()->create())
+    ->test(ShoppingCart::class)
+    ->call('addToCart', $product1)
+    ->call('addToCart', $product2);
+
+   $livewire->assertSee($product1->name);
+   $livewire->assertSee($product1->price);
+   $livewire->assertSee($product1->description);
+   $livewire->assertSee($product2->name);
+   $livewire->assertSee($product2->price);
+   $livewire->assertSee($product2->description);
+
+   $livewire->call('removeFromCart', $product1->id);
+   $livewire->assertdontsee($product1->name);
+   $livewire->assertdontsee($product1->description);
 });
