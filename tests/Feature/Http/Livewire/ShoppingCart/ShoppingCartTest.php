10fecbf3e49819d206bc76e158e0575b30fc6db4
RichardGL11 <richardgreghilammenda@hotmail.com>
2025-03-08 14:34:13 -0300

    test: it should add a product to the ShoppingCart

---
+++ tests/Feature/Http/Livewire/ShoppingCart/ShoppingCartTest.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Livewire\ShoppingCart\ShoppingCart;
+use Livewire\Livewire;
+
+it('should add a item to the shopping cart',function() {
+    $product1 =\App\Models\Product::factory()->create();
+    $product2 =\App\Models\Product::factory()->create();
+
+   $livewire = Livewire::actingAs(\App\Models\User::factory()->create())
+    ->test(ShoppingCart::class)
+    ->call('addToCart', $product1)
+    ->call('addToCart', $product2);
+
+   $livewire->assertSee($product1->name);
+   $livewire->assertSee($product1->price);
+   $livewire->assertSee($product1->description);
+   $livewire->assertSee($product2->name);
+   $livewire->assertSee($product2->price);
+   $livewire->assertSee($product2->description);
+
+});
