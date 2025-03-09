582e7b263baf22022e81aaaf438bbe4b088c6974
RichardGL11 <richardgreghilammenda@hotmail.com>
2025-03-09 13:45:40 -0300

    feature: Separting users in admin or normal User

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Enums\UserType;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
@@ -41,6 +42,12 @@ class UserFactory extends Factory
     {
         return $this->state(fn (array $attributes) => [
             'email_verified_at' => null,
+        ]);
+    }
+    public function admin(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'user_type' => UserType::ADMIN->value,
         ]);
     }
     public function getCPF()
--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('cpf')->unique();
             $table->string('phone')->unique();
             $table->string('email')->unique();
+            $table->string('user_type')->default(\App\Enums\UserType::USER->value);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
