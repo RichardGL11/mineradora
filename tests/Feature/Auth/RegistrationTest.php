b2ccc26f11b986238fb17c3d959e932c13173696
RichardGL11 <richardgreghilammenda@hotmail.com>
2025-04-13 14:02:35 -0300

    test: testing cpf rule

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -27,3 +27,19 @@ test('new users can register', function () {
 
     $this->assertAuthenticated();
 });
+test('validation cpf field', function ($rule, $value) {
+    $component = Volt::test('pages.auth.register')
+        ->set('name', 'Test User')
+        ->set('email', 'test@example.com')
+        ->set('cpf', $value)
+        ->set('phone', '11111111111')
+        ->set('password', 'password')
+        ->set('password_confirmation', 'password');
+
+    $component->call('register');
+    $component->assertHasErrors(['cpf' =>$rule ]);
+})->with([
+    'CPF min'    => ['O CPF informado é inválido precisa ter exatos 11 caracteres.', 123],
+    'CPF repeat' => ['O CPF informado é inválido seus números não podem ser todos iguais.','99999999999'],
+    'CPF valid' =>  ['O CPF informado é inválido.','99991233459'],
+]);
